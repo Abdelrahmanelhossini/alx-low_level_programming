46b2bbcc5fe90dbade3ee3362b9c0732e0699e3f
abdlerahman <bodyhoss54@gmail.com>
2023-03-21 02:34:04 +0200

    f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main -entry point
+ *
+ * Description:A C program that prints with printf fun
+ *
+ * Return: alawys 0 (success)
+ */
+
+int main(void)
+{
+	ptintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
