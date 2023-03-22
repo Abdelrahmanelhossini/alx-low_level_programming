c34605c4d73ebb8cef43b826ec789960c8cc65f7
abdlerahman <bodyhoss54@gmail.com>
2023-03-22 02:27:15 +0200

    f

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -entry point
+ *
+ * Descripyion: print _putchar using putchar prototype
+ *
+ * Return:always 0 (success)
+ */
+int main(void)
+{
+	char srt[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8 ; ch++)
+		_putchar(str[ch]);
+	_putchar("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MTHEADERS_H
+#define MYHEADERS_H
+
+#include<srdio.h>
+
+int _putchar(char c);
+
+
+#endif
