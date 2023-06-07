6427df8c1e6c314f61044e934401c25f2f7c5365
root <root@LAPTOP-JHEVLNIB.localdomain>
2023-06-08 02:39:47 +0300

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
-#ifndef UNTITLED35_M_H
-#define UNTITLED35_M_H
+#ifndef MTHEADERS_H
+#define MYHEADERS_H
 
-#endif //UNTITLED35_M_H
+#include<stdio.h>
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -12,4 +12,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 
+#endif
 
